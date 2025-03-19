85a0f720af15f57947e47ac008fc8478457218b2
maglore9900 <78699112+maglore9900@users.noreply.github.com>
2025-03-19 10:27:05 -0400

    Update adapter.py

--- modules/adapter.py
+++ modules/adapter.py
@@ -7,41 +7,41 @@ class Adapter:
         self.llm_text = env("LLM_TYPE")
         self.prompt = ChatPromptTemplate.from_template(prompts.main)
         if self.llm_text.lower() == "openai":
-            from langchain_openai import OpenAIEmbeddings
+            # from langchain_openai import OpenAIEmbeddings
             from langchain_openai import ChatOpenAI
             self.llm_chat = ChatOpenAI(
                 temperature=env.float("OPENAI_TEMP", 0.5), model=env("OPENAI_MODEL"), openai_api_key=env("OPENAI_API_KEY")
             )
-            self.embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
+            # self.embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
         elif self.llm_text.lower() == "local":
-            from langchain_huggingface import HuggingFaceEmbeddings
+            # from langchain_huggingface import HuggingFaceEmbeddings
             from langchain_ollama import ChatOllama
             self.llm_chat = ChatOllama(
                 base_url=env("OLLAMA_ENDPOINT"), model=env("OLLAMA_MODEL"), num_ctx=env.int("OLLAMA_TOKENS", 2048), temperature=env.float("OLLAMA_TEMPERATURE", 0.5)
             )
-            model_name = "BAAI/bge-small-en"
-            model_kwargs = {"device": "cpu"}
-            encode_kwargs = {"normalize_embeddings": True}
-            self.embedding = HuggingFaceEmbeddings(
-                model_name=model_name,
-                model_kwargs=model_kwargs,
-                encode_kwargs=encode_kwargs,
-            )
+            # model_name = "BAAI/bge-small-en"
+            # model_kwargs = {"device": "cpu"}
+            # encode_kwargs = {"normalize_embeddings": True}
+            # self.embedding = HuggingFaceEmbeddings(
+            #     model_name=model_name,
+            #     model_kwargs=model_kwargs,
+            #     encode_kwargs=encode_kwargs,
+            # )
         elif self.llm_text.lower() == "hybrid":
             from langchain_openai import OpenAIEmbeddings, OpenAI
-            from langchain_huggingface import HuggingFaceEmbeddings
+            # from langchain_huggingface import HuggingFaceEmbeddings
 
             self.llm = OpenAI(temperature=0, openai_api_key=env("OPENAI_API_KEY"))
-            model_name = "BAAI/bge-small-en"
-            model_kwargs = {"device": "cpu"}
-            encode_kwargs = {"normalize_embeddings": True}
-            self.embedding = HuggingFaceEmbeddings(
-                model_name=model_name,
-                model_kwargs=model_kwargs,
-                encode_kwargs=encode_kwargs,
-            )
+            # model_name = "BAAI/bge-small-en"
+            # model_kwargs = {"device": "cpu"}
+            # encode_kwargs = {"normalize_embeddings": True}
+            # self.embedding = HuggingFaceEmbeddings(
+            #     model_name=model_name,
+            #     model_kwargs=model_kwargs,
+            #     encode_kwargs=encode_kwargs,
+            # )
         else:
-            raise ValueError("Invalid LLM")
+            raise ValueError("Invalid LLM: Please set LLM_TYPE to either 'openai' or 'local'")
 
     def chat(self, query, chat_history):
         from langchain_core.output_parsers import StrOutputParser
